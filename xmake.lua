1ce9cf236bf6088c4f135a2fae4f005fa2d93d5d
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-30 04:44:42 -0600

    revert: release impl

--- xmake.lua
+++ xmake.lua
@@ -33,6 +33,3 @@ target("BakaQuickFullSaves")
 
     -- add extra files
     add_extrafiles(".clang-format")
-
-    -- add install files
-    add_installfiles("res/*.ini", { prefixdir = "SFSE/Plugins" })
